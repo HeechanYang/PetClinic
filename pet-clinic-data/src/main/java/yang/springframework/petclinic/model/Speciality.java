704fa8ad6fd6e7786bea12d42167aff5e909ee5f
HeechanYang <yhc944@gmail.com>
2018-11-18 17:18:43 +0900

    [ADD] Add Speciality Entity for Vet. closes #21

---
+++ pet-clinic-data/src/main/java/yang/springframework/petclinic/model/Speciality.java
@@ -0,0 +1,8 @@
+package yang.springframework.petclinic.model;
+
+import lombok.Data;
+
+@Data
+public class Speciality extends BaseEntity{
+    private String description;
+}
--- pet-clinic-data/src/main/java/yang/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/yang/springframework/petclinic/model/Vet.java
@@ -3,7 +3,10 @@ package yang.springframework.petclinic.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.Set;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class Vet extends Person{
+    private Set<Speciality> specialities;
 }
