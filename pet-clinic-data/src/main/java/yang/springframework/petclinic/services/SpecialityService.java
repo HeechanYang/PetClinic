aa48fdbfef021c092732bacd26454508bd587370
HeechanYang <yhc944@gmail.com>
2018-11-18 18:04:34 +0900

    [ADD] Add SpecialityServiceMap. closes #26

---
+++ pet-clinic-data/src/main/java/yang/springframework/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package yang.springframework.petclinic.services;
+
+import yang.springframework.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
