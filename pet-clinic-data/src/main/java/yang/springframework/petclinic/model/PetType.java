946adef6629bac786bfea66f6fa48968709c17a1
HeechanYang <yhc944@gmail.com>
2018-11-26 21:14:20 +0900

    [UPDATE] Updated base entity with JPA annotations.

--- pet-clinic-data/src/main/java/yang/springframework/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/yang/springframework/petclinic/model/PetType.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Entity;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class PetType extends BaseEntity{
