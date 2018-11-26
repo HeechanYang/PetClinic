f120340294efb98b43fb9f33e3745c45dbe89fbb
HeechanYang <yhc944@gmail.com>
2018-11-26 23:47:02 +0900

    [UPDATE] Convert Visit entity with JPA annotations.

--- pet-clinic-data/src/main/java/yang/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/yang/springframework/petclinic/model/Pet.java
@@ -5,6 +5,8 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 //Exclude Circular dependency
 @EqualsAndHashCode(exclude = "owner", callSuper = true)
@@ -25,4 +27,7 @@ public class Pet extends BaseEntity{
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 }
