7df2c4780c81756df02e50d04c04a14fcad7f443
HeechanYang <yhc944@gmail.com>
2018-11-26 22:06:32 +0900

    [UPDATE] Convert Vet entity with JPA annotations. close #32

--- pet-clinic-data/src/main/java/yang/springframework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/yang/springframework/petclinic/model/Speciality.java
@@ -2,7 +2,11 @@ package yang.springframework.petclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+
 @Data
 public class Speciality extends BaseEntity{
+
+    @Column(name = "description")
     private String description;
 }
--- pet-clinic-data/src/main/java/yang/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/yang/springframework/petclinic/model/Vet.java
@@ -3,11 +3,18 @@ package yang.springframework.petclinic.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id")
+    ,inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 }
